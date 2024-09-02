dec2a5b30c0a81cd84b55a7e8dbc7cc541839685
anna.coutinho_youcast <anna.coutinho_youcast@prestador.globo>
2024-09-02 20:08:42 -0300

    componente filter

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -1,5 +1,7 @@
 import HeaderElement from "../components/Header/header.lit.js";
-import SearchElement from "../components/Search/search.lit.js"
+import SearchElement from "../components/Search/search.lit.js";
+import FilterElement from "../components/Filter/filter.lit.js";
 
 customElements.define('eve-header', HeaderElement);
 customElements.define('eve-search', SearchElement);
+customElements.define('eve-filter', FilterElement);
